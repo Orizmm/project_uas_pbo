927523af914f4e544f0561e5402ec707239e4595
Carneyval <pramudiar101@gmail.com>
2024-07-08 21:13:54 +0700

    ComboBox fakultas&prodi, Menu pendidikan akhir

--- src/models/Prodi.java
+++ src/models/Prodi.java
@@ -33,6 +33,10 @@ public class Prodi {
         return nama_prodi;
     }
 
+    @Override
+    public String toString(){
+        return nama_prodi;
+    }
     /**
      * @return the id_fakultas
      */
